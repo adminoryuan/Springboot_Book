c9713d27a3e413f7e2ef5d2091198425a1c86142
yuan <1317332365@qq.com>
2022-05-18 13:10:04 +0800

    init

--- Webchat/src/main/java/com/example/webchat/server/Websocket/IHandleMessage.java
+++ Webchat/src/main/java/com/example/webchat/server/Websocket/IHandleMessage.java
@@ -2,7 +2,9 @@ package com.example.webchat.server.Websocket;
 
 //import org.springframework.web.socket.WebSocketSession;
 
+import org.springframework.web.socket.WebSocketSession;
+
 public interface IHandleMessage {
 
-    //void Handle(String body, WebSocketSession session);
+    void Handle(String body, WebSocketSession session);
 }
--- Webchat/src/main/java/com/example/webchat/server/Websocket/WebsocketServere.java
+++ Webchat/src/main/java/com/example/webchat/server/Websocket/WebsocketServere.java
@@ -1,83 +1,84 @@
 package com.example.webchat.server.Websocket;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.socket.CloseStatus;
-//import org.springframework.web.socket.WebSocketHandler;
-//import org.springframework.web.socket.WebSocketMessage;
-//import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-//public class WebsocketServere implements WebSocketHandler {
-//
-//
-//    private Map<String,WebSocketSession> OnLineMap=new ConcurrentHashMap<>();
-//
-//    @Autowired
-//    IHandleMessage server;
-//
-//    /**
-//     * 返回当前在线人数
-//     * @return
-//     */
-//    public int GetOnlineProson(){
-//        return OnLineMap.size();
-//
-//    }
-//
-//    /**
-//     * 连接时触发
-//     * @param session
-//     * @throws Exception
-//     */
-//    @Override
-//    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-//
-//    }
-//
-//    /**
-//     * 收到消息时触发
-//     * @param session
-//     * @param message
-//     * @throws Exception
-//     */
-//    @Override
-//    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
-//        String Body= message.getPayload().toString();
-//
-//        server.Handle(Body,session);
-//
-//    }
-//
-//    /**
-//     * 发生异常时回调
-//     * @param session
-//     * @param exception
-//     * @throws Exception
-//     */
-//    @Override
-//    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
-//
-//    }
-//
-//    /**
-//     * 关闭链接时
-//     * @param session
-//     * @param closeStatus
-//     * @throws Exception
-//     */
-//    @Override
-//    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
-//
-//    }
-//
-//    /**
-//     * 是否支持消息分片
-//     * @return
-//     */
-//    @Override
-//    public boolean supportsPartialMessages() {
-//        return false;
-//    }
-//}
+public class WebsocketServere implements WebSocketHandler {
+
+
+    private Map<String,WebSocketSession> OnLineMap=new ConcurrentHashMap<>();
+
+    @Autowired
+    IHandleMessage server;
+
+    /**
+     * 返回当前在线人数
+     * @return
+     */
+    public int GetOnlineProson(){
+        return OnLineMap.size();
+
+    }
+
+    /**
+     * 连接时触发
+     * @param session
+     * @throws Exception
+     */
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+
+    }
+
+    /**
+     * 收到消息时触发
+     * @param session
+     * @param message
+     * @throws Exception
+     */
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+        String Body= message.getPayload().toString();
+
+
+        server.Handle(Body,session);
+
+    }
+
+    /**
+     * 发生异常时回调
+     * @param session
+     * @param exception
+     * @throws Exception
+     */
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+
+    }
+
+    /**
+     * 关闭链接时
+     * @param session
+     * @param closeStatus
+     * @throws Exception
+     */
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+
+    }
+
+    /**
+     * 是否支持消息分片
+     * @return
+     */
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+}
